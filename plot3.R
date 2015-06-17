7ee6edea598f41aca632459900e3ef4fdd3bad83
Thiago Veras <tveras@motorola.com>
2015-06-17 12:35:06 -0300

    third commit

---
+++ plot3.R
@@ -0,0 +1,40 @@
+# Question 3:
+# Of the four types of sources indicated by the type (point, nonpoint, onroad, 
+# nonroad) variable, which of these four sources have seen decreases in emissions 
+# from 1999–2008 for Baltimore City? 
+# Which have seen increases in emissions from 1999–2008?
+plot3 <- function() 
+{ 
+
+# Using ggplot2 library
+library(ggplot2)    
+
+# Read data files 
+message("Reading NEI Data")    
+NEI <- readRDS("nei-data/summarySCC_PM25.rds")
+message("Reading SCC Data")
+SCC <- readRDS("nei-data/Source_Classification_Code.rds")
+
+# Filtering the data to Baltimore City, Maryland (fips == "24510")
+baltimore.emissions <- NEI[NEI$fips=="24510",]
+# Aggregating the emissions by year to analize the data and answer the question
+baltimore.emissions.aggr <- aggregate(Emissions ~ year + type,
+                                  data=baltimore.emissions,
+                                  FUN=sum)
+
+# Ploting the graph
+message("Ploting the graph")
+ggplot(baltimore.emissions.aggr, aes(x=factor(year), y=Emissions, fill=type)) +
+    geom_bar(stat="identity") +
+    facet_grid(. ~ type) +
+    xlab("year") +
+    ylab(expression("total PM"[2.5]*" emission")) +
+    ggtitle(expression("PM"[2.5]*paste(" emissions in Baltimore ",
+                                       "City by various source types", sep="")))
+
+## Saving it to a PNG file
+message("Saving the PNG file (plot3.png)")
+dev.copy(png, file="plot3.png", height=480, width=640)
+dev.off()
+
+}
